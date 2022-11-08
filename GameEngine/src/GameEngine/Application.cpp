3d490be6c5de0a4f6e1232642a5619edda3e7c43
Thijs Zijlstra <thijszijlstra@icloud.com>
2022-11-08 14:01:25 +0100

    Added event system + basic test

--- GameEngine/src/GameEngine/Application.cpp
+++ GameEngine/src/GameEngine/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Events\ApplicationEvent.h"
+#include "Log.h"
+
 namespace GameEngine {
 
 	Application::Application()
@@ -14,6 +17,16 @@ namespace GameEngine {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			GE_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			GE_TRACE(e);
+		}
+
 		while (true);
 	}
 
--- GameEngine/src/GameEngine/Core.h
+++ GameEngine/src/GameEngine/Core.h
@@ -9,3 +9,5 @@
 #else
 	#error Game Engine only supports Windows!
 #endif // GE_PLATFORM_WINDOWS
+
+#define BIT(x) (1 << x)
